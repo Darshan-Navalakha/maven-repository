f3784b8c7de36e0931b7953fd1880f8221524232
Darshan Navalakha <darshannavalakha.04@gmail.com>
2020-12-21 15:16:54 +0530

    added division

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -11,10 +11,14 @@ public class Calc {
 	public static int multiply(int n1,int n2) {
 		return n1*n2;
 	}
+	public static int divide(int n1,int n2) {
+		return n1/n2;
+	}
 	
 	public static void main(String args[]) {
 		System.out.println("add "+add(10,20));
 		System.out.println("subs "+subs(10,20));
 		System.out.println("production "+multiply(10,20));
+		System.out.println("quotient "+divide(10,20));
 	}
 }
