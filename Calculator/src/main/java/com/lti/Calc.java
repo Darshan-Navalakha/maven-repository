d9349ca91f6d80398953c1a752b3167e3e662923
Darshan Navalakha <darshannavalakha.04@gmail.com>
2020-12-21 15:09:42 +0530

    added multiply

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,9 +8,13 @@ public class Calc {
 	public static int subs(int n1,int n2) {
 		return n1-n2;
 	}
+	public static int multiply(int n1,int n2) {
+		return n1*n2;
+	}
 	
 	public static void main(String args[]) {
 		System.out.println("add "+add(10,20));
 		System.out.println("subs "+subs(10,20));
+		System.out.println("production "+multiply(10,20));
 	}
 }
